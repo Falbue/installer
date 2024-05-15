08df62a345d6df7ae79f9bbe294276cee6bf251b
Falbue <cyansair05@gmail.com>
2024-05-15 23:04:57 +0500

    Теперь это глобальные функции

--- installer.py
+++ installer.py
@@ -35,6 +35,44 @@ def latest_version(repo):
 latest_version = latest_version(repository)
 print(f"Текущая версия: {version}")
 
+
+def create_folder(folder_name):
+    user_folder = os.path.expanduser('~')  # Получаем путь к папке пользователя
+    install_path = os.path.join(user_folder, 'Falbue')
+    # Создаем папку для сохранения файлов на рабочем столе.
+    folder_path = os.path.join(install_path, folder_name)
+    os.makedirs(folder_path, exist_ok=True)
+    return folder_path
+
+def download_files_from_github(app, folder_path):
+    def download_file(file_url, file_name): 
+        with open(file_name, 'wb') as f:
+            f.write(requests.get(file_url).content)
+        print(f"Загрузка: {file_name}")
+
+    def download_files_from_github_in_dir(dir_name, dir_path): 
+        api_url = f"https://api.github.com/repos/{git_nick}/{app}/contents/{dir_name}"
+        response = requests.get(api_url)
+        files_in_dir = response.json()
+        
+        for file_in_dir in files_in_dir:
+            if file_in_dir['type'] == 'file': 
+                file_path = os.path.join(dir_path, file_in_dir['name'])
+                download_file(file_in_dir['download_url'], file_path)
+
+    api_url = f"https://api.github.com/repos/{git_nick}/{app}/contents"
+    response = requests.get(api_url)
+    files = response.json()
+    
+    for file in files:
+        file_path = os.path.join(folder_path, file['name'])
+        if file['type'] == 'file': 
+            download_file(file['download_url'], file_path)
+        elif file['type'] == 'dir': 
+            os.makedirs(file_path, exist_ok=True)
+            download_files_from_github_in_dir(file['path'], file_path)
+
+
 if version == str(latest_version):
     print("Последняя верия")
 
